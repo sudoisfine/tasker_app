8f364c1520f9d031a72d2dd736ceb10cf367ef31
MELISSA GUION <M.GUION@GMAIL.COM>
2016-05-19 08:45:27 -0500

    update hover on tasks with jQuery function to change icon and styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,23 @@
 //= require bootstrap-datepicker
 //= require turbolinks
 //= require_tree .
+
+function hoverIn(event){
+  $(event.target).addClass('text-muted');
+
+  $(event.target).closest('a').children('i')
+    .removeClass('fa-square-o')
+    .addClass('fa-check-square-o');
+}
+
+function hoverOut(event){
+  $(event.target).removeClass('text-muted');
+
+  $(event.target).closest('a').children('i')
+    .removeClass('fa-check-square-o')
+    .addClass('fa-square-o');
+}
+
+$(document).ready(function(){
+  $('.task').hover(hoverIn, hoverOut);
+});
